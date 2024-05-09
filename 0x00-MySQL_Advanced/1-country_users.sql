5bd0b9b9b76bea69334a851ac8d608bffa933337
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-05-09 15:52:12 +0000

    a SQL script that creates a table users following these requirements:
    With these attributes:
    id, integer, never null, auto increment and primary key
    email, string (255 characters), never null and unique
    name, string (255 characters)
    country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
    If the table already exists, your script should not fail
    Your script can be executed on any database

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,6 @@
 -- a SQL script that creates a table users
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
