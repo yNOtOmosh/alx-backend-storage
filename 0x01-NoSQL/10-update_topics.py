7e47151b2eaf6c6a113178ad502d3f3d12f02678
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-05-14 09:47:50 +0000

    a Python function that changes all topics of a school document based on the name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Change school topics."""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update topics for a school."""
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
